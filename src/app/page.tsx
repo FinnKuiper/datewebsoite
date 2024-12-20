846c1716705ddc63aa5e98cefe657fce87eefd34
Finn Kuiper <finnkuiper05@gmail.com>
2024-12-20 23:20:36 +0100

    add NavBar component and typography elements; update layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { TypographyH1 } from '@/typography/TypographyH1';
+import { TypographyH2 } from '@/typography/TypographyH2';
+import { TypographyP } from '@/typography/TypographyP';
+
 export default function Home() {
   return (
     <>
-      <h1>womp womp</h1>
+      <TypographyH1>Home</TypographyH1>
+      <TypographyP>W dates!</TypographyP>
+      <TypographyH2>Featured dates!</TypographyH2>
+      <TypographyP>&quot;deze gaan echt hard 💯💯&quot; - iedereen</TypographyP>
+      <Carousel className="w-full flex flex-row max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </>
   );
 }
